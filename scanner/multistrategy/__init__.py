4dbfb8062c3c579c21bad46e6facc932fce3abfe
lloydsatkinson <lloyd.s.atkinson@googlemail.com>
2026-08-29 14:09:33 -0400

    feat: export multi-strategy study

---
+++ scanner/multistrategy/__init__.py
@@ -0,0 +1,4 @@
+from .config import MultiStrategyConfig
+from .study import MultiStrategyStudy, broad_candidate_context, opening_baseline_for_day
+
+__all__ = ["MultiStrategyConfig", "MultiStrategyStudy", "broad_candidate_context", "opening_baseline_for_day"]
